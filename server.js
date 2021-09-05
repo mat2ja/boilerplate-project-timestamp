8d20d251702af1b799c3a1c1cc1c44049d777d40
Matija Osrečki <matija.osrecki@gmail.com>
2021-09-05 12:14:51 +0200

    Inital commit :tada:

--- server.js
+++ server.js
@@ -1,32 +1,66 @@
-// server.js
-// where your node app starts
+const express = require('express');
+const dateFns = require('date-fns');
 
-// init project
-var express = require('express');
-var app = express();
+const { isValid } = dateFns;
+
+const app = express();
+
+const PORT = process.env.PORT || '3000';
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api', (req, res) => {
+	try {
+		const now = Date.now();
+		res.send({
+			unix: now,
+			utc: new Date(now),
+		});
+	} catch (error) {
+		res.status(400).send({ error: 'Invalid Date' });
+	}
 });
 
+app.get('/api/:date', (req, res) => {
+	try {
+		const parsedDate = Number.isInteger(+req.params.date)
+			? new Date(+req.params.date)
+			: new Date(req.params.date);
 
+		if (isValid(parsedDate)) {
+			const unix = Number.isInteger(+req.params.date)
+				? +req.params.date
+				: parsedDate.valueOf();
+
+			return res.send({
+				unix,
+				utc: parsedDate,
+			});
+		}
+
+		throw new Error();
+	} catch (error) {
+		res.status(400).send({ error: 'Invalid Date' });
+	}
+});
+
+// your first API endpoint...
+app.get('/api/hello', (req, res) => {
+	res.json({ greeting: 'hello API' });
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(PORT, () => {
+	console.log(`Your app is listening on port ${listener.address().port}`);
 });
